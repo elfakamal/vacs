226572e2f0537b08f4e1aa316aa2a3a18946f3b8
Kamal Farsaoui <elfa.kamal@gmail.com>
2017-09-23 10:51:10 +0200

    clean reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,27 +1,17 @@
-import {
-  Action,
-  ReducersMapObject,
-  State,
-} from 'common';
+import { Action, ReducersMapObject, State } from 'common';
 
 import { CONVERSATIONS_SUCCESS, ConversationSuccessAction } from '../actions';
 import { INITIAL_STATE } from '../constants';
 
-const handleActions = (cases: ReducersMapObject) => {
-  return (state: State = INITIAL_STATE, action: Action<string>): State => {
-    if (!action || !cases[action.type]) {
-      return state;
-    }
-
-    return cases[action.type](state, action);
-  };
-};
+const handleActions = (cases: ReducersMapObject) => (
+  (state: State = INITIAL_STATE, action: Action<string>): State => (
+    (!action || !cases[action.type]) ? state : cases[action.type](state, action)
+  )
+);
 
 export default handleActions({
-  [CONVERSATIONS_SUCCESS]: (state: State, action: ConversationSuccessAction): State => {
-    return {
-      ...state,
-      conversations: action.conversations,
-    };
-  },
+  [CONVERSATIONS_SUCCESS]: (state: State, action: ConversationSuccessAction): State => ({
+    ...state,
+    conversations: action.conversations,
+  }),
 });
